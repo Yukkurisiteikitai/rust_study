f2fbc4375b1893e7dfa2eb60196f997b56e91122
Yukkurisiteikitai <YukkuriorSience@gmail.com>
2026-04-18 13:56:01 +0900

    feat: reset

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,5 @@
 use std::io;
 
 fn main() {
-    let mut input = String::new();
-    io::stdin()
-        // 多分これはread_lineでpythonのinput("")みたいなことができて第一引数で、指定先のやつをやっているのかなって思った。
-        .read_line(&mut input)
-        .expect("読み込みに失敗しました。");
-    println!("{}", input);
     println!("Hello, world!");
 }
